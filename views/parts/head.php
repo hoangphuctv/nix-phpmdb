ba43f788f5b7eae4f1d2333c443be260ecd246ea
PhucTH <tv.hoangphuc@gmail.com>
2019-03-18 10:03:16 +0700

    fix link css file

--- views/parts/head.php
+++ views/parts/head.php
@@ -3,7 +3,7 @@
 	<title><?php echo isset($post->title) ? $post->title . " - ": ""; echo $config->site_name ?></title>
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link href="/static/css/app.css"/>
+	<link rel="stylesheet" href="/static/css/app.css">
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	
 	<!-- Global site tag (gtag.js) - Google Analytics -->
