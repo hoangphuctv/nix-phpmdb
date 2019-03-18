f20168568b1837d74304b79f05a58847538d1b30
PhucTH <tv.hoangphuc@gmail.com>
2019-03-18 14:31:43 +0700

    change page navigate to number

--- views/home.php
+++ views/home.php
@@ -6,10 +6,10 @@
 			<?php include __DIR__ . '/parts/list2.php'; ?>
 			
 			<?php if ($page > 1) { ?>
-				<a class="btn btn-primary" href="?page=<?= $page - 1 ?>"> &lt; Prev page</a>
+				<a class="btn btn-primary" href="?page=<?= $page - 1 ?>"> &lt; Page <?= $page - 1 ?></a>
 			<?php } ?>
 			<?php if ($page < $total_page) { ?>
-				<a class="btn btn-primary" href="?page=<?= $page + 1 ?>">  Next page &gt;</a>
+				<a class="btn btn-primary" href="?page=<?= $page + 1 ?>"> Page <?= $page + 1 ?> &gt;</a>
 			<?php } ?>
 			<?php include __DIR__ . '/parts/footer.php'; ?>	
 		</div>
